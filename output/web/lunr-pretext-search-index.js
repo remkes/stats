86981dc1af7b3c8a95d871f4aab001e07d981410
remkes <remkes.kooistra@kingsu.ca>
2026-07-27 16:16:05 -0600

    complete the structure and setup

---
+++ output/web/lunr-pretext-search-index.js
@@ -0,0 +1,302 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "front-colophon",
+  "level": "1",
+  "url": "front-colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "section-structure",
+  "level": "1",
+  "url": "section-structure.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Course Structure",
+  "body": " Course Structure  How the course is structured.  "
+},
+{
+  "id": "section-organization",
+  "level": "1",
+  "url": "section-organization.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Course Learning Outomes",
+  "body": " Course Learning Outomes  This course is organized into learning outcomes, and each section of the notes (and each video) reflection one of the learning outcomes. There are three types of these: core learning outcomes (CLOs), stretch learning outcomes (SLOs) and holistic learning outcomes (HLOs). The main content of the course is organized by the CLOs and the SLOs: you will see each section of the course labelled by one of these. The HLOs are major ideas, themes and skills that run throughout the course but are not isolated to any one area. I'll try to point out the development of the HLOs over the whole course.  Here are the learning outcomes for the course. These are all stated with active verbs: a learning outcome is something that a student will be able to do, which can be demonstrated and assessed. You can preface all of these with A student completing this course should be able to .  Here are the 20 core learning outcomes.   Effectively use the vocabulary of a quantitative study and a data set. In that data set, distinguish between variables and cases. Classify types of variable in a data set.  Analyze and critique the design of a quantitative study, including discussion of variables, hypotheses, sampling, scope of inference, biases and counfounding variables.  Analyze ethical aspects of study design, data collection, data interpretation and publication of statistical results.  Construct and analyze frequency and contingency tables for categorical data, including discussion of potential hypothesis.  Construct and analyze various bar plots and pie charts as visualizations of categorical data.  Construct and analyze histograms for a single numeric variable, using statistical vocaulbary (skew, modality, outliers, etc) to describe the shape of the data.  Calculate and analyze measures of central tendency and spread to describe a single numeric variables and its historgram.  Construct and analyze scatterplots for two numeric variables, focusing on potential relationships and hypotheses about relationships.  Construct a linear regression for the relationship between two numeric variable. Interpret the coefficients of such a linear regression.  Use residuals, correlations, and to analyze the fit of a linear model.  State, construct and critique a null and alternative hypotheses for a research questions, including questions for simple, double or multiples variables and questions for both categorical and numeric variables. Determine what kind of statistical inference is necessary to test these hypotheses.  Describe the idea of randomness and probability as potential source for a distribution of data and the relationship of randomness to an alternative hypothesis for a research question.  Calculate and analyze a sampling distribution for a difference of two proportions. Using this sampling distribution, calculate and analyze -scores and -values.  Describe the properties of the normal distribution and how it is produced by random selection for a difference of proportions.  Calculate and analyze a confidence interval in a sampling distribution for difference of proporties.  Define and identify Type 1 and Type 2 errors in study design, using the language of hypothesis testing for a difference in proportions.  Conduct and interpret hypothesis tests and construct confidence intervals for a single proportion.  Conduct and interpret hypothesis tests for difference within several categorical variables, showing understanding and use of the distribution.  Conduct and interpret hypothesis tests and confidence intervals for a single mean.  Conduct and interpret hypothesis tests and confidence intervals for a difference in means.  Conduct and interpret hypothesis tests and confidence intervals for multiple means using ANOVA.  Conduct and interpret hypothesis tests and confidence intervals for the coefficient of a linear regression model.   Here are the 5 stretch learning outcomes.   Describe the basic principles and first mathematical constructions of the theory of probability.  Use probability formulas to construct random sampling distributions.  Use the language of areas under curves and the symbols of integral calculus to mathematically express percentiles and probabilities.  Use the mathematical formulation of a general normal distribution to calculate -score, percentiles and other aspects of a sampling distribution.  Describe the mathematical properties and use of t-distributions and distributions.   "
+},
+{
+  "id": "section-vocabulary",
+  "level": "1",
+  "url": "section-vocabulary.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-study-design",
+  "level": "1",
+  "url": "section-study-design.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-study-ethics",
+  "level": "1",
+  "url": "section-study-ethics.html",
+  "type": "Section",
+  "number": "2.3",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-tables",
+  "level": "1",
+  "url": "section-tables.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-bar-pie",
+  "level": "1",
+  "url": "section-bar-pie.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-histograms",
+  "level": "1",
+  "url": "section-histograms.html",
+  "type": "Section",
+  "number": "3.3",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-central-tendencies",
+  "level": "1",
+  "url": "section-central-tendencies.html",
+  "type": "Section",
+  "number": "3.4",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-scatterplots",
+  "level": "1",
+  "url": "section-scatterplots.html",
+  "type": "Section",
+  "number": "3.5",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-linear-regression",
+  "level": "1",
+  "url": "section-linear-regression.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-linear-analysis",
+  "level": "1",
+  "url": "section-linear-analysis.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-hypotheses",
+  "level": "1",
+  "url": "section-hypotheses.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-randomness",
+  "level": "1",
+  "url": "section-randomness.html",
+  "type": "Section",
+  "number": "5.2",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-probability-theory",
+  "level": "1",
+  "url": "section-probability-theory.html",
+  "type": "Section",
+  "number": "5.3",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-sampling-distributions",
+  "level": "1",
+  "url": "section-sampling-distributions.html",
+  "type": "Section",
+  "number": "5.4",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-normal-distributions",
+  "level": "1",
+  "url": "section-normal-distributions.html",
+  "type": "Section",
+  "number": "5.5",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-mahtmeatics-sampling-distributions",
+  "level": "1",
+  "url": "section-mahtmeatics-sampling-distributions.html",
+  "type": "Section",
+  "number": "5.6",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-slo3-areas-under-curves",
+  "level": "1",
+  "url": "section-slo3-areas-under-curves.html",
+  "type": "Section",
+  "number": "5.7",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-mathematics-normal-distributions",
+  "level": "1",
+  "url": "section-mathematics-normal-distributions.html",
+  "type": "Section",
+  "number": "5.8",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-confidence-intervals",
+  "level": "1",
+  "url": "section-confidence-intervals.html",
+  "type": "Section",
+  "number": "5.9",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-errors",
+  "level": "1",
+  "url": "section-errors.html",
+  "type": "Section",
+  "number": "5.10",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-ht-single-proportion",
+  "level": "1",
+  "url": "section-ht-single-proportion.html",
+  "type": "Section",
+  "number": "6.1",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-ht-chi-squared",
+  "level": "1",
+  "url": "section-ht-chi-squared.html",
+  "type": "Section",
+  "number": "6.2",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-mathematics-t-chi-squared",
+  "level": "1",
+  "url": "section-mathematics-t-chi-squared.html",
+  "type": "Section",
+  "number": "6.3",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-ht-single-mean",
+  "level": "1",
+  "url": "section-ht-single-mean.html",
+  "type": "Section",
+  "number": "6.4",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-ht-difference-means",
+  "level": "1",
+  "url": "section-ht-difference-means.html",
+  "type": "Section",
+  "number": "6.5",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-ht-anova",
+  "level": "1",
+  "url": "section-ht-anova.html",
+  "type": "Section",
+  "number": "6.6",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "section-ht-linear",
+  "level": "1",
+  "url": "section-ht-linear.html",
+  "type": "Section",
+  "number": "6.7",
+  "title": "Section Name",
+  "body": " Section Name  Test  "
+},
+{
+  "id": "chapter-glossary",
+  "level": "1",
+  "url": "chapter-glossary.html",
+  "type": "Chapter",
+  "number": "7",
+  "title": "Glossary",
+  "body": " Glossary   This is a glossary of terms in the course.     Inferential Statistics  text    Descriptive Statistics  text    "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
